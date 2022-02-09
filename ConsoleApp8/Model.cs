d4655c7391137cb979d4eb37d307b4115ed80b29
Dartware <dartware@mail.ru>
2022-02-09 15:40:47 +0300

    Interfaces

---
+++ ConsoleApp8/Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+	public sealed class Model : IObject, IService
+	{
+		void DoWork()
+		{
+			Console.WriteLine(nameof(IObject));
+		}
+
+		void DoWork()
+		{
+			Console.WriteLine(nameof(IService));
+		}
+
+	}
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp8
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
